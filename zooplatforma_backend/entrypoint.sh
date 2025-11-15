ce79750ba855e512fc4ce385e62bfae35d92c997
Pavel Okhrim <paokhrim@gmail.com>
2025-11-15 20:55:26 +0300

    Refactor Dockerfile and entrypoint script for improved execution and permissions

--- zooplatforma_backend/entrypoint.sh
+++ zooplatforma_backend/entrypoint.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 mkdir -p /app/static /app/media /app/logs
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
 exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
